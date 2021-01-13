6c91648a3162de7a6ba063c78024f2cabeb9832f
Alexandre <alexbx2002@gmail.com>
2021-01-13 15:54:01 +0100

    Story 4
    Trié par date de sortie

--- Main.js
+++ Main.js
@@ -1,4 +1,5 @@
-let movie = require("./" + "movies.json");
+const { table } = require('console');
+
 let args = process.argv;
 
 
@@ -7,7 +8,7 @@ function transform(input,output){
         let FullDate = new Date((input[i].release_date)*1000);
         let Year = FullDate.getFullYear();
         input[i].title = input[i].title + " " +"(" + Year + ")";
-        nouveauJson( input[i], output)
+        nouveauJson( input[i], output);
     }
 }
 
@@ -17,6 +18,20 @@ function nouveauJson(ajout, out){
     fs.appendFileSync(out,writable);
 }
 
+function sort_date(input,output){
+    for(i=input.length-1;i>=1;i--){
+         for(j=0;j<=i-1;j++){
+             if( input[j+1].release_date < input[j].release_date){
+                 let tmp = input[j];
+                 input[j]= input[j+1];
+                 input[j+1] = tmp;
+             }
+         }
+    }
+    for(i=0; i<input.length; i++){
+        nouveauJson( input[i], output);
+    }
+ }
 
 
 for (i = 0 ; i < process.argv.length ; i++){
@@ -24,7 +39,18 @@ for (i = 0 ; i < process.argv.length ; i++){
         if (args[i+1] == 'transform'){
             let input = require(args[i+2]);
             let output = args[i+3];
+            start = new Date().getTime();
             transform(input,output);
+            stop = new Date().getTime();
+            console.log("time : " + (stop - start));
+        }
+        if (args[i+1] == 'sort_date'){
+            let input = require(args[i+2]);
+            let output = args[i+3];
+            start = new Date().getTime();
+            sort_date(input,output);
+            stop = new Date().getTime();
+            console.log("time : " + (stop - start));
         }
     }
 }
