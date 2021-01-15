244846a18b46386ca93aafcb0119862dce5f48e8
mattmax1 <matthieukoebsch@hotmail.com>
2021-01-15 10:48:15 +0100

    Story 5
    Renvoie tout les film sortie une année donnée et prend en compte si le fichier json est trier ou pas

--- Main.js
+++ Main.js
@@ -46,32 +46,110 @@ function sort_titre(input,output){
    }
 }
 
+function search_date_false(input,search_year){
+    for(i=0 ; i<input.length ; i++){
+        let FullDate = new Date((input[i].release_date)*1000);
+        let Year = FullDate.getFullYear();
+        if(Year == search_year){
+            console.log(input[i])
+        }
+    }
+}
+
+function search_date_true(input,search_year){
+    let min = 0;
+    let max = input.length;
+    let middle = (min+max)/2;
+    if (middle % 2 != 0){
+        middle = middle-0.5;
+    }
+    let FullDate = new Date((input[middle].release_date)*1000);
+    let middle_year = FullDate.getFullYear()
+    while(search_year != middle_year){
+        if(middle_year<search_year){
+            min = middle;
+            middle = (min+max)/2;
+            if (middle % 1 != 0){
+                middle = middle-0.5;
+            }
+            FullDate = new Date((input[middle].release_date)*1000);
+            middle_year = FullDate.getFullYear()
+        }
+        if(middle_year>search_year){
+            max = middle;
+            middle = (min+max)/2;
+            if (middle % 1 != 0){
+                middle = middle-0.5;
+            }
+            FullDate = new Date((input[middle].release_date)*1000);
+            middle_year = FullDate.getFullYear()
+        }
+    }
+    while(middle_year == search_year){
+        middle = middle-1;
+        FullDate = new Date((input[middle].release_date)*1000);
+        middle_year = FullDate.getFullYear()
+    }
+    middle = middle + 1;
+    FullDate = new Date((input[middle].release_date)*1000);
+    middle_year = FullDate.getFullYear()
+    while(middle_year == search_year){
+        console.log(input[middle]) ;
+        middle = middle+1;
+        FullDate = new Date((input[middle].release_date)*1000);
+        middle_year = FullDate.getFullYear()
+    }
+}
+
+function search_key_word(input,key_word,genre){
+    for(i=0;i<input.length;i++){
+        for(j=0;j<input[i].genres;j++)
+            if(input[i].genres == genre){
+                console.log("salut")
+                console.log(input[i])
+            }
+
+    }
+}
+
+
+
+
+
+
 
 for (i = 0 ; i < process.argv.length ; i++){
     if (args[i] == '-action'){
+        let input = require(args[i+2]);
+        start = new Date().getTime();
         if (args[i+1] == 'transform'){
-            let input = require(args[i+2]);
             let output = args[i+3];
-            start = new Date().getTime();
             transform(input,output);
-            stop = new Date().getTime();
-            console.log("time : " + (stop - start));
         }
         if (args[i+1] == 'sort_date'){
-            let input = require(args[i+2]);
             let output = args[i+3];
-            start = new Date().getTime();
             sort_date(input,output);
-            stop = new Date().getTime();
-            console.log("time : " + (stop - start));
         }
         if (args[i+1] == 'sort_titre'){
-            let input = require(args[i+2]);
             let output = args[i+3];
-            start = new Date().getTime();
             sort_titre(input,output);
-            stop = new Date().getTime();
-            console.log("time : " + (stop - start));
         }
+        if (args[i+1] == 'search_date'){
+            let search_year = args[i+3];
+            if (args[i+4] == 'false'){
+                search_date_false(input,search_year);  
+            }
+            if (args[i+4] == 'true'){  
+                search_date_true(input,search_year);
+            }
+        }
+        if (args[i+1] == 'search_key_word'){
+            let key_word = args[i+3];
+            let genre = args[i+4];
+            search_key_word(input,key_word,genre);
+        }
+        
+        stop = new Date().getTime();
+        console.log("time : " + (stop - start));
     }
 }
